9dc445d0c2bcfe225712ade42737022b66896929
yjing <shanxiaji@163.com>
2017-12-07 13:44:06 +0800

    1 添加私有maven仓库的例子；

--- MavenDemoProject/app/src/main/java/com/yoryky/mavendemo/MainActivity.java
+++ MavenDemoProject/app/src/main/java/com/yoryky/mavendemo/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.gson.Gson;
+import com.yoryky.androidlib.utils.FileUtil;
 import com.yoryky.androidlib.utils.HelpUtil;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         HelpUtil.showToast(this,"hello world");
         Gson gson = new Gson();
+        try{
+            FileUtil.writeExternal(this,"hello.txt","测试写入一点东西");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
